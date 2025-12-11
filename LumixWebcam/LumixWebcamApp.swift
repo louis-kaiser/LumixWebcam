12a98e4b5680c70e2d02a99ed8226bedf901a5b7
louis-kaiser <louis.kaiser0203@icloud.com>
2025-12-11 12:39:46 +0100

    Initial Commit

---
+++ LumixWebcam/LumixWebcamApp.swift
@@ -0,0 +1,17 @@
+//
+//  LumixWebcamApp.swift
+//  LumixWebcam
+//
+//  Created by Louis Kaiser on 11.12.25.
+//
+
+import SwiftUI
+
+@main
+struct LumixWebcamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
